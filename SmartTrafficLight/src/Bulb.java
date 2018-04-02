c41026a25dd5926ceff049358eb569f79d405319
NayreetIslam <nayreet.islam@uoit.ca>
2018-04-02 09:28:21 -0400

    Initial Commit
    This is an initial Commit of SMART TRAFFIC LIGHT

---
+++ SmartTrafficLight/src/Bulb.java
@@ -0,0 +1,62 @@
+import java.awt.Color;
+
+public class Bulb {
+
+	private boolean on;
+	private Color color;
+	private String colorName;
+	
+	//constructor
+	public Bulb(boolean on) {
+		this.on=on;
+	}
+	public Bulb(boolean on,Color c, String cn) {
+		this.on=on;
+		color=c;
+		colorName=cn;
+	}
+	
+	
+	public boolean isOn(){
+		return on;
+	}
+	
+	public boolean isOff(){
+		return !on;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color c) {
+		color=c;
+	}
+	
+	public String getColorName() {
+		return colorName;
+	}
+	public void setColorName(String cn) {
+		colorName=cn;
+	}
+	
+	
+	public void turnOn() {
+		on=true;
+	}
+	
+	public void turnOff() {
+		on=false;
+	}
+	
+	public String toString() {
+		String result;
+		if(on) {
+			result="The "+getColorName()+" light is on.";
+		}
+		else {
+			result="The "+getColorName()+" light is off.";
+		}
+		return result;
+	}
+	
+	
+}
---
+++ SmartTrafficLight/src/TrafficLight.java
@@ -0,0 +1,62 @@
+import java.awt.Color;
+
+public class TrafficLight {
+	//instance fields
+	private Bulb[] lights;
+	
+	
+	//constructors
+	public TrafficLight() {
+		lights = new Bulb[3];
+		lights[0]= new Bulb(true, Color.RED,"red");
+		lights[1]= new Bulb(false, Color.YELLOW,"yellow");
+		lights[2]= new Bulb(false, Color.GREEN,"green");
+	}
+	
+	//asscessors and mutators
+	
+	public Bulb[] getLights() {
+		return lights;
+	}
+	public void setLights(Bulb[] lights) {
+		this.lights=lights;
+	}
+	
+	//algorithm for changing the traffic lights
+	
+	public void nextState() {
+		// red to green
+		if(lights[0].isOn()) {
+			lights[0].turnOff();
+			lights[2].turnOn();
+		}
+		// green to yellow
+		else if(lights[2].isOn()) {
+			lights[2].turnOff();
+			lights[1].turnOn();
+		}
+		//yellow to red
+		else if(lights[1].isOn()) {
+			lights[1].turnOff();
+			lights[0].turnOn();
+		}
+	}
+	
+	//index of bulb that is lit
+	public int indexOfLitBulb() {
+		int i=0;
+		while(i < lights.length && lights[i].isOff()) {
+			i++;
+		}
+		return i;
+	}
+	
+	public String toString() {
+		String result="";
+		for(int i=0;i<lights.length;i++) {
+			result+="Bulb "+(i+1)+" is "+lights[i].getColorName()+" and "
+					+ lights[i].toString().toLowerCase()+"\n";
+		}
+		return result;
+	}
+}
