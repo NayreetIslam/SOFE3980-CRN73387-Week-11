10a97073061d4f09b96396a5d2916667a54917c8
cmfabregas <cmfabregas1@gmail.com>
2018-04-04 11:13:22 -0400

    Commit from Dev A

--- SmartTrafficLight/src/TrafficLightPanel.java
+++ SmartTrafficLight/src/TrafficLightPanel.java
@@ -49,27 +49,24 @@ public class TrafficLightPanel extends JPanel implements Runnable{
 		page.setColor(Color.LIGHT_GRAY);
 		page.fillRect(X, Y, WIDTH, HEIGHT);
 		//draw the red light
-		if(light.indexOfLitBulb()==0) {
+		
+		switch(light.indexOfLitBulb()) {
+		case 0:
 			page.setColor(Color.RED);
-		}else {
-			page.setColor(Color.DARK_GRAY);
-		}
-		page.fillOval(X+X_OFFSET, Y+Y_OFFSET, DIAMETER, DIAMETER);
-		
-		//draw the yellow light
-		if(light.indexOfLitBulb()==1) {
+			break;
+		case 1:
 			page.setColor(Color.YELLOW);
-		}else {
-			page.setColor(Color.DARK_GRAY);
-		}
-		page.fillOval(X+X_OFFSET, Y+DIAMETER+2*Y_OFFSET, DIAMETER, DIAMETER);
-		
-		//draw the green light
-		if(light.indexOfLitBulb()==2) {
+			break;
+		case 2:
 			page.setColor(Color.GREEN);
-		}else {
+			break;
+		default:
 			page.setColor(Color.DARK_GRAY);
+			break;
 		}
+		
+		page.fillOval(X+X_OFFSET, Y+Y_OFFSET, DIAMETER, DIAMETER);
+		page.fillOval(X+X_OFFSET, Y+DIAMETER+2*Y_OFFSET, DIAMETER, DIAMETER);
 		page.fillOval(X+X_OFFSET, Y+2*DIAMETER+3*Y_OFFSET, DIAMETER, DIAMETER);
 		
 		page.setColor(Color.BLACK);
@@ -96,20 +93,22 @@ public class TrafficLightPanel extends JPanel implements Runnable{
 		
 		for(;;) {
 			try {
-				if(light.indexOfLitBulb()==0) {
+				
+				switch(light.indexOfLitBulb()) {
+				case 0:
 					Thread.sleep(lane2Time);
 					signalName="GO";
-					//System.out.println(lane2Time);
-				}
-				else if(light.indexOfLitBulb()==1) {
+					break;
+				case 1:
 					Thread.sleep(1000);
 					signalName="STOP";
-				}	
-				else if(light.indexOfLitBulb()==2) {
+					break;
+				case 2:
 					Thread.sleep(lane1Time);
 					signalName="SLOW";
-					//System.out.println(lane1Time);
+					break;
 				}
+				
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
